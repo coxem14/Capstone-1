6bdec73ca68f220cd891b28a665441f130ce4a94
Erin Cox <coxem14@gmail.com>
2020-10-30 13:26:11 -0700

    adding a bunch of images

--- movies.py
+++ movies.py
@@ -19,9 +19,6 @@ df_str_to_literal - takes in a dataframe and column_name string and returns the 
 
 dict_name_cleaner_ - takes in a dictionary, checks if the parameter is actually a dictionary, and if so returns a list of values corresponding to the 'name' key in that dictionary. If it is not a dictionary, it returns an empty list.
 
-
-
-
 '''
 
 
@@ -64,21 +61,17 @@ def budget_cleaner(string):
     return 0
 
 
-
 '''
 Manipulating DataFrames Helper Functions
 
-get_counts_df - takes in a dataframe with columns containing lists and returns a new dataframe with the items in the list exploded and reindexed to get total counts of the values in the lists across the entire dataframe. Can graph counts vs values easily.
-
-
 '''
 
-def get_counts_df(df, column):
+def get_counts_df(df, column, idx):
     column_df = pd.DataFrame(df[column])
     column_counts = column_df[column].explode().value_counts()
     column_counts_df = column_counts.to_frame('counts').reset_index()
     column_counts_df = column_counts_df.rename(columns = {'index': column})
-    
+    column_counts_df = column_counts_df[column_counts_df.index <= idx]
     return column_counts_df
 
 def create_bool_columns(df, column_name, list_of_columns):
@@ -90,7 +83,6 @@ def broadcast_and_clean(df, broadcast_col, list_of_columns):
     
     for column in list_of_columns:
         out_df[column] = out_df[column]*out_df[broadcast_col]
-    
     out_df = out_df.drop(columns = [broadcast_col])
     out_df = out_df.replace(0.0,np.nan)
     
@@ -100,7 +92,8 @@ def get_top(df, num, column, list_of_columns):
     out_df = df.nlargest(num, column)[list_of_columns]
     return out_df
 
-
+def make_genre_specific_df(df, genre):
+    return df[df[genre] == 1]
 
 '''
 Plotting Helper Functions
@@ -137,12 +130,6 @@ def violin_plot_by_genre(data, x_labels, y_label, title):
     ax.set_xticklabels(x_labels, fontsize = 20, rotation=45)
     sns.violinplot(data=data, ax=ax)
     fig.tight_layout()
-
-
-
-
-
-
 
 
 '''
@@ -189,116 +176,23 @@ def qq_plot(X, target, results, title='QQ Plot vs Normal Distribution for Residu
     stats.probplot(target - y_predict, plot=ax)
     ax.set_title(title, fontsize = 18)
 
+def get_xvar(list_of_variables):
+    d = {}
+    for idx, var in enumerate(list_of_variables):
+        d[idx] = var
+    return d
 
+def create_new_var_list(d, list_of_idx):
+    new_var_list = []
+    for idx in list_of_idx:
+        if (idx - 1) in d:
+            new_var_list.append(d[(idx -1)])
+    return new_var_list
 
-
-
-
-
-
-'''
-Genres_cleaner - returns the value for the 'name' key in each dictionary in the list of dictionaries when mapped to a column of a dataframe
-'''    
-
-
-genres_lists = str_to_list_of_dicts(df = genres_df, column_name = 'genres').map(genres_cleaner)
-#print(genres_lists.head())
-
-genres_counts = genres_lists.explode().value_counts()
-#print(genres_counts)
-
-genres_counts_df = genres_counts.to_frame('counts').reset_index()
-genres_counts_df = genres_counts_df.rename(columns = {'index': 'genre'})
-#print(genres_counts_df)
-
-genres_counts_df = genres_counts_df[genres_counts_df.counts > 1]
-#print(genres_counts_df)
-
-
-'''
-Generate Bool Columns - creates boolean columns in the dataframe for each unique item in list of columns
-'''
-
-def create_bool_columns(df, column_name, list_of_columns):
-    for column in list_of_columns:
-        df[column] = df[column_name].apply(lambda x: column in x)
-
-
-genres_list = genres_counts_df['genre'].tolist()
-#print(genres_list)
-
-create_bool_columns(df = movies_meta, column_name = 'genres', list_of_columns = genres_list)
-#print(movies_meta.head())
-
-'''
-Helper function to create genre specific dataframes - I tried to automate this by feeding in a list of genres and desired datafame names and zipping them together to return dataframes, but I couldn't get it work.
-'''
-
-def make_genre_specific_df(df, genre):
-    return df[df[genre] == True]
-
-# drama = make_genre_specific_df(movies_meta, 'Drama')
-# comedy = make_genre_specific_df(movies_meta, 'Comedy')
-# thriller = make_genre_specific_df(movies_meta, 'Thriller')
-# romance = make_genre_specific_df(movies_meta, 'Romance')
-# action = make_genre_specific_df(movies_meta, 'Action')
-# horror = make_genre_specific_df(movies_meta, 'Horror')
-# crime = make_genre_specific_df(movies_meta, 'Crime')
-# documentary = make_genre_specific_df(movies_meta, 'Documentary')
-# adventure = make_genre_specific_df(movies_meta, 'Adventure')
-# science_fiction = make_genre_specific_df(movies_meta, 'Science Fiction')
-# family = make_genre_specific_df(movies_meta, 'Family')
-# mystery = make_genre_specific_df(movies_meta, 'Mystery')
-# fantasy = make_genre_specific_df(movies_meta, 'Fantasy')
-# animation = make_genre_specific_df(movies_meta, 'Animation')
-# foreign = make_genre_specific_df(movies_meta, 'Foreign')
-# music = make_genre_specific_df(movies_meta, 'Music')
-# history = make_genre_specific_df(movies_meta, 'History')
-# war = make_genre_specific_df(movies_meta, 'War')
-# western = make_genre_specific_df(movies_meta, 'Western')
-# tv_movie = make_genre_specific_df(movies_meta, 'TV Movie')
-
-
-'''
-Clean Production Companies, Production Countries, Spoken Languages
-'''
-
-prod_companies_df = pd.DataFrame(movies_meta['production_companies'])
-prod_countries_df = pd.DataFrame(movies_meta['production_countries'])
-spoken_language_df = pd.DataFrame(movies_meta['spoken_languages'])
-
-prod_companies_lists = str_to_list_of_dicts(df = prod_companies_df, column_name = 'production_companies').map(genres_cleaner)
-
-def countries_cleaner(list_of_dicts):
-    if isinstance(list_of_dicts, list):
-        return [d['iso_3166_1'] for d in list_of_dicts]
-    return []
-
-def language_cleaner(list_of_dicts):
-    if isinstance(list_of_dicts, list):
-        return [d['iso_639_1'] for d in list_of_dicts]
-    return []
-
-countries_lists = str_to_list_of_dicts(df = prod_countries_df, column_name = 'production_countries').map(countries_cleaner)
-#print(countries_lists.head())
-
-language_lists = str_to_list_of_dicts(df = spoken_language_df, column_name = 'spoken_languages').map(language_cleaner)
-#print(language_lists.head())
-
-'''
-Budget Cleaner - takes budget column which was a string object and converts it to an int
-'''
-
-def budget_cleaner(string):
-    if string.isnumeric():
-        val = int(string)
-        return val
-    
-    #in the case of missing or non-numeric data (there was a .jpg in this column for some reason)
-    return 0
-
-
-
+def get_p_values_idx(results):
+    p_values = np.array(results.pvalues)
+    p_values_idx = np.argwhere(p_values < 0.05).reshape(1,-1).tolist()[0]
+    return p_values_idx
 
 
 
